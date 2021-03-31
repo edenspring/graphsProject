5b1b1987a6cdf67837afd985c5c979a2ab7d2d0d
bsnernier <nbern817@gmail.com>
2021-03-31 14:39:14 -0500

    complete num_region

--- 02_graphsProject/lib/num_regions.js
+++ 02_graphsProject/lib/num_regions.js
@@ -1,22 +1,37 @@
 function numRegions(graph) {
-    let regions = [];
-    let bigSet = new Set();
+    // let regions = [];
+    // let bigSet = new Set();
+
+    // for (key in graph) {
+    //     let neighbors = graph[key];
+    //     let current = [key];
+    //     let currentSet = new Set(key);
+    //     neighbors.forEach(neighbor => {
+    //         if (!currentSet.has(neighbor)) {
+    //             currentSet.add(neighbor);
+    //         }
+    //     })
+    //     if (!bigSet.has(currentSet)) {
+    //         bigSet.add(currentSet);
+
+    //     }
+    // }
+    // return bigSet.size;
+
+    let counter = 0;
+    let visited = new Set();
 
     for (key in graph) {
-        let neighbors = graph[key];
-        let current = [key];
-        let currentSet = new Set(key);
-        neighbors.forEach(neighbor => {
-            if (!currentSet.has(neighbor)) {
-                currentSet.add(neighbor);
-            }
-        })
-        if (!bigSet.has(currentSet)) {
-            bigSet.add(currentSet);
-
+        if (!visited.has(key)) {
+            counter++
+            visited.add(key)
         }
+        let neighbors = graph[key];
+        neighbors.forEach(neighbor => {
+            visited.add(neighbor)
+        })
     }
-    return bigSet.size;
+    return counter
 }
 
 module.exports = {
